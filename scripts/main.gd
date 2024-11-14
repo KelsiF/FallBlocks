4f57264c6f1d97e0b44e1ba883fcd96810ca1dc3
Kelsi <superigromen2018@gmail.com>
2024-11-14 22:39:17 +0300

    v. 1.1.0-dev

--- scripts/main.gd
+++ scripts/main.gd
@@ -4,10 +4,13 @@ signal playerDead
 
 var playerIsDead = false
 var meteor = preload("res://meteor.tscn")
+var buff = preload("res://buff.tscn")
+var debuff = preload("res://debuff.tscn")
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	spawn_meteors(0)
 	Main.playerDead.connect(_on_player_dead)
+	
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -20,11 +23,30 @@ func _on_player_dead():
 
 func spawn_meteors(count):
 	var rng = RandomNumberGenerator.new()
+	var chanceBuff = 1
+	var buffSpawned = false
+	
 	
 	while playerIsDead == false:
-		var x = rng.randf_range(65, 1216)
+		var x = rng.randf_range(0, 1270)
 		var y = rng.randf_range(-70, -900)
 		var instance = meteor.instantiate()
-		instance.position = Vector2(x+rng.randf_range(-20, 20), y+rng.randf_range(-20, 20))
-		await get_tree().create_timer(rng.randf_range(0.1, 1)).timeout
-		add_child(instance)		
+		instance.position = Vector2(x, y)
+		await get_tree().create_timer(rng.randf_range(0.1, 0.5)).timeout
+		add_child(instance)
+		
+		chanceBuff = rng.randi_range(1, 100)
+		if (chanceBuff >= 10):
+			buffSpawned = true
+		if (buffSpawned):
+			var buffOrDebuff = rng.randi_range(1, 100)
+			if (buffOrDebuff >= 50):
+				var instanceBuff = buff.instantiate()
+				instanceBuff.position = Vector2(rng.randf_range(0, 1270), rng.randf_range(-70, -900))
+				await get_tree().create_timer(0.5)
+				add_child(instanceBuff)
+			elif (buffOrDebuff < 50):
+				var instanceDebuff = debuff.instantiate()
+				instanceDebuff.position = Vector2(rng.randf_range(0, 1270), rng.randf_range(-70, -900))
+				await get_tree().create_timer(0.5)
+				add_child(instanceDebuff)
--- scripts/meteor.gd
+++ scripts/meteor.gd
@@ -16,6 +16,10 @@ func _on_body_entered(body: Node) -> void:
 	if body.name == "Player":
 		await call_deferred("queue_free")
 		Main.playerDead.emit()
-		pass
 	if body.name == "Area2D":
+		$Sprite2D.hide()
+		$CollisionShape2D.call_deferred("queue_free")
+		$CPUParticles2D.call_deferred("queue_free")
+		$breakingParticles.emitting = true
+		await get_tree().create_timer(1.0).timeout
 		await call_deferred("queue_free")
